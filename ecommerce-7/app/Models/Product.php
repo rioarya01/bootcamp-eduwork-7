315b6c7d407f4a67fdc3a6d96a33e2fc1447a585
rioarya01 <rio283931@gmail.com>
2026-03-14 22:06:29 +0800

    Upload tugas-13

--- ecommerce-7/app/Models/Product.php
+++ ecommerce-7/app/Models/Product.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'product_category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo
+        (ProductCategory::class, 'product_category_id');
+    }
 }
--- ecommerce-7/database/2026_03_06_060717_create_products_table.php
+++ ecommerce-7/database/2026_03_06_060717_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps(); // menambahkan kolom created_at dan updated_at
         });
     }
 
